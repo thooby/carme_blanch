63ac8c9c45df4c815512c1317afb07727d48972c
Juan Zubillaga <juan@zubi.net>
2011-12-02 17:45:14 +0100

    Permite passwords y usuarios sin tener en cuenta mayúsculas y minusculas

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,12 @@
 class SessionsController < ApplicationController
+  
+  
   def new
   end
 
   def create
-    user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
+    user = User.find_by_email(params[:email].downcase)
+    if user && user.authenticate(params[:password].downcase)
       session[:user_id] = user.id
       redirect_to root_url, :notice => "Logged in!"
     else
@@ -17,4 +19,6 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to root_url, :notice => "Logged out!"
   end
+  private
+ 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ActiveRecord::Base
+class User < ActiveRecord::Base 
   ROLES = %w[admin operador socio banned]
   attr_accessible :email, :password, :password_confirmation, :role
   has_secure_password
