95cf37e73b494047acdeb3b02765fbde90753b4d
Juan Zubillaga <juan@zubi.net>
2011-12-18 15:48:49 +0100

    Seeds y limpiar primera página para socios

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   ROLES = %w[admin operador socio banned]
   attr_accessible :email, :password, :password_confirmation, :role
   has_secure_password
-  validates_presence_of :password, :on => :create
+  #validates_presence_of :password, :on => :create
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,14 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
-users = User.create([{:id => 1, :email => 'admin', :password_digest => '', :role => 'admin'}])
-var_confs =VarConf.create([{:id => 1, :titulo =>'Versión genérica', :name_foto => 'carme.gif'}])
+require 'digest' 
+new_user = User.create([{
+  :id => 1,
+  :email => 'admin', 
+  :password => 'admin', 
+  :password_confirmation => 'admin',
+  :role => 'admin'}])  
+  new_user.save
+  User.create(:email => "admin", :role => "admin@example.com")
+var_confs = VarConf.create([{:id => 1, :titulo =>'Versión genérica', :name_foto => 'carme.gif'}])
 tipos = Tipo.create([{:id =>1, :descripcion => 'Ingreso'},{:id => 2, :descripcion => 'Gasto'}])
